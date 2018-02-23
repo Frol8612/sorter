f56a05cff9f0a2833efe8566d210bd5c52c1cc58
Frol8612 <31982517+Frol8612@users.noreply.github.com>
2018-02-23 23:34:13 +0300

    sort

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
 class Sorter {
   constructor() {
-    // your implementation
+    // your implementa
+    this.mass = new Array();
+    this.compareFunction = function (x, y) {
+      return x - y;
+    };
   }
 
   add(element) {
     // your implementation
+    this.mass.push(element);
   }
 
   at(index) {
-    // your implementation
+    // your implementatio
+    return this.mass[index];
   }
 
   get length() {
     // your implementation
+    return this.mass.length;
   }
 
   toArray() {
     // your implementation
+    return this.mass;
   }
 
   sort(indices) {
     // your implementation
+    let sor = this.mass.filter((elem, pos) => indices.includes(pos)).sort(this.compareFunction);
+
+    this.mass.forEach((elem, pos) => {
+      if (indices.includes(pos)) {
+        this.mass[pos] = sor.shift();
+      }
+    });
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
